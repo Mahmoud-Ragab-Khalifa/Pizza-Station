e8ef8b51741326baf4638a04cfc67661a656342b
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-03-26 01:18:47 +0200

    move reusable menu components from best-sellers to menu directory

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import MenuItem from "../BestSellers/MenuItem";
+import MenuItem from "./MenuItem";
 
 const Menu = ({ items }: { items: any }) => {
   return (
