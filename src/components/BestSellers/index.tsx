c5bb496c6b129575c47919e1d65ae1bba9c0cc3a
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-03-30 02:36:12 +0200

    add product sizes in prisma schema and display real products

--- src/components/BestSellers/index.tsx
+++ src/components/BestSellers/index.tsx
@@ -1,31 +1,10 @@
+import { db } from "@/lib/prisma";
 import MainHeading from "../MainHeading";
 import Menu from "../Menu";
 
-const bestSellersList = [
-  {
-    id: crypto.randomUUID(),
-    name: "Pizza",
-    description: "This Is A Pizza",
-    basePrice: 12,
-    image: "/assets/images/pizza.png",
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Pizza",
-    description: "This Is A Pizza",
-    basePrice: 12,
-    image: "/assets/images/pizza.png",
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Pizza",
-    description: "This Is A Pizza",
-    basePrice: 12,
-    image: "/assets/images/pizza.png",
-  },
-];
+const BestSellers = async () => {
+  const bestSellersList = await db.product.findMany();
 
-const BestSellers = () => {
   return (
     <section className="section-gap">
       <div className="container">
