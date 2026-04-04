df82868ca0c06ccb10f28b6b0c6ed171227b72f0
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-04-04 07:53:06 +0200

    display real cart items count in header cart-button

--- src/components/Header/CartButton.tsx
+++ src/components/Header/CartButton.tsx
@@ -2,9 +2,12 @@
 
 import Link from "../Link";
 import { Routes } from "@/constants/enums";
+import { selectCartTotalItems } from "@/redux/features/cart/cartSlice";
+import { useAppSelector } from "@/redux/hooks";
 import { ShoppingCartIcon } from "lucide-react";
 
 const CartButton = () => {
+  const totalItems = useAppSelector(selectCartTotalItems);
   return (
     <Link
       href={Routes.CART}
@@ -12,7 +15,7 @@ const CartButton = () => {
       aria-label="Cart Page Button"
     >
       <span className="absolute -top-4 inset-s-4 w-5 h-5 text-sm bg-primary rounded-full text-white text-center">
-        2
+        {totalItems}
       </span>
 
       <ShoppingCartIcon
