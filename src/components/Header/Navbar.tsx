509c3833b8aaea890e8024bce2c7c9c2dc568292
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-04-06 22:16:45 +0200

    highlight active navigation link based on current route

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -7,25 +7,41 @@ import { Button, buttonVariants } from "../ui/button";
 import { useState } from "react";
 import { Pages, Routes } from "@/constants/enums";
 import ThemeToggler from "../Theme/ThemeToggler";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  const pathname = usePathname();
+
+  console.log(pathname);
 
   return (
     <nav>
       <ul
         className={`z-50 flex flex-col md:flex-row absolute inset-s-0 md:inset-auto md:top-auto w-full md:w-auto md:static md:items-center gap-4 py-5 md:p-0 transition-all duration-500 top-full overflow-hidden ${isOpenMenu ? "max-h-64" : "max-h-0 md:max-h-full p-0!"} bg-muted md:bg-transparent`}
       >
-        {NAV_LINKS.map(({ title, href }) => (
-          <li key={href}>
-            <Link
-              href={href}
-              className={`px-4 block md:px-3 py-2 font-semibold ${href === `${Routes.AUTH}${Pages.LOGIN}` ? `${buttonVariants({ size: "lg" })} px-8! mx-4 md:m-0` : "text-accent hover:text-primary transition-colors duration-300"}`}
-            >
-              {title}
-            </Link>
-          </li>
-        ))}
+        {NAV_LINKS.map(({ title, href }) => {
+          const isAuth = href === `${Routes.AUTH}${Pages.LOGIN}`;
+          const isActive = href === pathname;
+
+          return (
+            <li key={href}>
+              <Link
+                href={href}
+                className={`px-4 block md:px-3 py-2 font-semibold ${
+                  isAuth
+                    ? `${buttonVariants({ size: "lg" })} px-8! mx-4 md:m-0`
+                    : `hover:text-primary transition-colors duration-300 ${
+                        isActive ? "text-primary" : "text-accent"
+                      }`
+                }`}
+              >
+                {title}
+              </Link>
+            </li>
+          );
+        })}
 
         <div className="flex items-center absolute inset-e-0 bottom-6 gap-4 mx-4 md:hidden md:p-0 md:m-0">
           <Button variant={"outline"} className="rounded-md">
