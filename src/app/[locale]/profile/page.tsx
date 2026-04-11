c4cf07c53e16a629340c3bd3da7378e6ad9faaf5
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-04-11 18:42:00 +0200

    update middleware to handle all protected and auth routes

--- src/app/[locale]/profile/page.tsx
+++ src/app/[locale]/profile/page.tsx
@@ -1,16 +1,4 @@
-import { Pages, Routes } from "@/constants/enums";
-import { authOptions } from "@/server/auth";
-import { getServerSession } from "next-auth";
-import { getLocale } from "next-intl/server";
-import { redirect } from "next/navigation";
-
 const ProfilePage = async () => {
-  const session = await getServerSession(authOptions);
-
-  const locale = await getLocale();
-
-  if (!session) redirect(`/${locale}${Routes.AUTH}${Pages.LOGIN}`);
-
   return <main>ProfilePage</main>;
 };
 
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,53 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "@/i18n/routing";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import withAuth from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { Locale } from "next-intl";
+import { Pages, Routes } from "./constants/enums";
 
 const intlMiddleware = createMiddleware(routing);
 
-export default async function Proxy(req: NextRequest) {
-  const response = intlMiddleware(req);
+export default withAuth(
+  async function Proxy(req: NextRequest) {
+    const response = intlMiddleware(req);
 
-  return response;
-}
+    const isAuth = await getToken({ req });
+
+    const pathname = req.nextUrl.pathname;
+    const locale = req.url.split("/")[3] as Locale;
+
+    const isAuthPage = pathname.startsWith(`/${locale}${Routes.AUTH}`);
+
+    const protectedRoutes = [Routes.PROFILE, Routes.ADMIN];
+    const isProtectedRoute = protectedRoutes.some((route) =>
+      pathname.startsWith(`/${locale}${route}`),
+    );
+
+    // if not loggedin user try to access protected page redirect it to login page
+    if (!isAuth && isProtectedRoute) {
+      return NextResponse.redirect(
+        new URL(`/${locale}${Routes.AUTH}${Pages.LOGIN}`, req.url),
+      );
+    }
+
+    // if logged in user try yo access auth page this action make user conflict so redirect to profile page
+    if (isAuth && isAuthPage) {
+      return NextResponse.redirect(
+        new URL(`/${locale}${Routes.PROFILE}`, req.url),
+      );
+    }
+
+    return response;
+  },
+  {
+    callbacks: {
+      authorized() {
+        return true;
+      },
+    },
+  },
+);
 
 export const config = {
   matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
