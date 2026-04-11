aa81586f2e9424a4731b02cc382accbad948038a
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-04-11 17:42:57 +0200

    update middleware structure to handle pages protection

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,15 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "@/i18n/routing";
+import { NextRequest } from "next/server";
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default async function Proxy(req: NextRequest) {
+  const response = intlMiddleware(req);
+
+  return response;
+}
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
   matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
