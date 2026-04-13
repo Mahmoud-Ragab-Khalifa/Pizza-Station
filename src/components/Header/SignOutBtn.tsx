f79f74ce8b72511d5cb666acc6e6f1621592383a
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-04-13 09:55:27 +0200

    add user role in schema and handle redirect of log out

--- src/components/Header/SignOutBtn.tsx
+++ src/components/Header/SignOutBtn.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { Button } from "../ui/button";
 import { signOut } from "next-auth/react";
+import { Pages, Routes } from "@/constants/enums";
 
 const SignOutBtn = () => {
   const t = useTranslations("navbar");
 
+  const locale = useLocale();
+
   return (
-    <Button className="lg:px-8" variant={"secondary"} onClick={() => signOut()}>
+    <Button
+      className="lg:px-8"
+      variant={"default"}
+      onClick={() =>
+        signOut({ callbackUrl: `/${locale}${Routes.AUTH}${Pages.LOGIN}` })
+      }
+    >
       {t("signOut")}
     </Button>
   );
