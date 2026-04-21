b88605871613f8eb258eb1c09bac7ff238e3acd9
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-04-21 14:34:54 +0200

    handle editing user from users list with cache revalidation

---
+++ src/app/[locale]/admin/users/[userId]/edit/page.tsx
@@ -0,0 +1,40 @@
+import EditUserForm from "@/components/EditUserForm";
+import { Pages, Routes } from "@/constants/enums";
+import { getAppTranslations } from "@/lib/getAppTranslations";
+import { getUser, getUsers } from "@/server/db/users";
+import { Locale } from "next-intl";
+import { redirect } from "next/navigation";
+
+export async function generateStaticParams() {
+  const users = await getUsers();
+
+  return users.map((user) => ({ userId: user.id }));
+}
+
+const EditUserPage = async ({
+  params,
+}: {
+  params: Promise<{ locale: Locale; userId: string }>;
+}) => {
+  const { locale, userId } = await params;
+
+  const user = await getUser(userId);
+
+  if (!user) {
+    redirect(`/${locale}${Routes.ADMIN}${Pages.USERS}`);
+  }
+
+  const translations = await getAppTranslations(locale);
+
+  return (
+    <main>
+      <section className="section-gap">
+        <div className="container">
+          <EditUserForm translations={translations} user={user} />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default EditUserPage;
