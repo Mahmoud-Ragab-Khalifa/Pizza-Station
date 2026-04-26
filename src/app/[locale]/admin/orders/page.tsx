01fbe3c949bd3471542eba403dc7bdaebc42df00
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-04-26 16:56:07 +0300

    feat: add initial structure for admin orders page to prevent runtime errors

---
+++ src/app/[locale]/admin/orders/page.tsx
@@ -0,0 +1,15 @@
+const OrdersPage = () => {
+  return (
+    <main>
+      <section className="section-gap">
+        <div className="container">
+          <h1 className="text-accent text-center text-sm">
+            Working On Orders Page
+          </h1>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default OrdersPage;
